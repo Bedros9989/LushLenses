8dc7b672f52ef08bce8a02c0bd2b65238220b9de
JossanGus <105631856+JossanGus@users.noreply.github.com>
2023-09-14 17:38:41 +0200

    hje

---
+++ kontakt.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("myForm"); 
+    
+
+        // Hämta formulärets data
+        const firstName = document.getElementById("firstName");
+        const lastName = document.getElementById("lastName");
+        const phoneNumber = document.getElementById("phoneNumber");
+        const email = document.getElementById("email");
+        const message = document.getElementById("message");
+
+        
+        
