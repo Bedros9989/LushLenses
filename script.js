ac86f46326b276076ae1950236a8c53a85a036eb
Bedros9989 <121031209+Bedros9989@users.noreply.github.com>
2023-09-18 12:27:23 +0200

    -

--- script.js
+++ script.js
@@ -58,31 +58,6 @@ function showSlides(n) {
 };
 
 
-/*********************************
-Produkter
-**********************************/
-
-// Initialize Vue instance
-const app = Vue.createApp({
-  data() {
-    return {
-      products: []
-    };
-  },
-  mounted() {
-    // Fetch product data from JSON file
-    fetch('products.json')
-      .then(response => response.json())
-      .then(data => {
-        this.products = data;
-      })
-      .catch(error => console.error('Error fetching products:', error));
-  }
-});
-
-// Mount the Vue instance on the app element
-app.mount('#app');
-
 
 
 
